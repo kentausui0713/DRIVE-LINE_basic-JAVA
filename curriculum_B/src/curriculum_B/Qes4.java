5d6ee447694a74b2706c5508a82dc307eaf010a1
kentausui0713 <usuikenta100@gmail.com>
2024-01-17 22:29:36 +0900

    [finish]Qes4

---
+++ curriculum_B/src/curriculum_B/Qes4.java
@@ -0,0 +1,18 @@
+package curriculum_B;
+
+public class Qes4 {
+
+	public static void main(String[] args) {
+		for(int i = 1; i < 10; i++) {
+			for(int j = 1; j < 10; j++) {
+				if(j == 9) {
+					System.out.println(String.format("%02d", i) + " * " + String.format("%02d",j) + " = " + String.format("%02d",i*j));
+				} else {
+					System.out.print(String.format("%02d", i) + " * " + String.format("%02d",j) + " = " + String.format("%02d",i*j) + " || ");
+				}
+			}
+		}
+
+	}
+
+}
